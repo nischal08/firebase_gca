3a85a37d10d97d25896c8888668e135c25b21aeb
nischal08 <nischal.karanjit@gmail.com>
2021-01-02 17:20:09 +0545

    implemented sqflite

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_gca/sqlite/sqliteExample.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:sqflite/sqflite.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.blue, systemNavigationBarColor: Colors.blue));
   runApp(MyApp());
 }
 
@@ -13,16 +17,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    _createDatabase();
+
     return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: SqliteExample(),
     );
+  }
+
+  _createDatabase() async {
+    String databasePath = await getDatabasesPath();
+    String databaseFileName = "my_db.db";
+    int databaseVersion = 1;
+    openDatabase(databasePath + "/" + databaseFileName,
+        version: databaseVersion, onCreate: (db, version) {
+      String createTableQuery =
+          "CREATE TABLE user(id INTEGER,firstname TEXT,lastname TEXT)";
+      db.execute(createTableQuery);
+    }, onUpgrade: (db, oldV, newV) {});
   }
 }
--- lib/sqlite/sqliteExample.dart
+++ lib/sqlite/sqliteExample.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
 
 class SqliteExample extends StatefulWidget {
   @override
@@ -17,6 +18,76 @@ class _SqliteExampleState extends State<SqliteExample> {
   }
 
   Widget _body() {
-    return Container();
+    return Container(
+      width: double.infinity,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _insertBtn(),
+          _readBtn(),
+          _updateBtn(),
+          _deleteBtn(),
+        ],
+      ),
+    );
+  }
+
+  Widget _insertBtn() {
+    return MaterialButton(
+      onPressed: () {
+        _insert();
+      },
+      child: Text("Insert"),
+      color: Colors.green,
+    );
+  }
+
+  _insert() async {
+    Database database = await _openDb();
+    Map<String, dynamic> userData = {
+      "id": 1,
+      "firstname": "John",
+      "lastname": "Doe"
+    };
+    await database.insert("user", userData);
+  }
+
+  Widget _readBtn() {
+    return MaterialButton(
+      onPressed: () async {
+        Database database = await _openDb();
+        List result = await database.query("user");
+        print(result);
+      },
+      child: Text("Read"),
+      color: Colors.green,
+    );
+  }
+
+  Widget _updateBtn() {
+    return MaterialButton(
+      onPressed: () async {
+        Database database = await _openDb();
+        await database.update("user", {"id": 2}, where: "id=?", whereArgs: [1]);
+      },
+      child: Text("Update"),
+      color: Colors.green,
+    );
+  }
+
+  Widget _deleteBtn() {
+    return MaterialButton(
+      onPressed: () async {
+        Database database = await _openDb();
+        database.delete("user", where: "id=?", whereArgs: [2]);
+      },
+      child: Text("Delete"),
+      color: Colors.green,
+    );
+  }
+
+  Future<Database> _openDb() async {
+    return openDatabase(await getDatabasesPath() + "/my_db.db");
   }
 }
